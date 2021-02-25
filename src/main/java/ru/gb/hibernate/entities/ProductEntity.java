9cd2d88c599d1d4d83c3508afcffe6e24c6b6d09
IgorTeslenko <igor.teslenko@gmail.com>
2021-02-25 20:11:09 +0100

    homework5 minor fix

--- src/main/java/ru/gb/hibernate/entities/ProductEntity.java
+++ src/main/java/ru/gb/hibernate/entities/ProductEntity.java
@@ -2,28 +2,25 @@ package ru.gb.hibernate.entities;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "products")
 public class ProductEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
     private Long id;
 
-    @Column
+    @NonNull
     private String title;
 
-    @Column
+    @NonNull
     private float price;
-
-    public ProductEntity(String title, float price) {
-        this.title = title;
-        this.price = price;
-    }
 }
